70f041614631a37c61981fdf4f36ef64b59a987a
Nelson <ahlinelson04@gmail.com>
2025-11-25 00:57:53 +0700

    merapikan

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -19,15 +19,14 @@ y = df['Churn']
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-input_example = X_train[0:5]
 n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
 max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
 
 with mlflow.start_run(run_name=f"elastic_search_{n_estimators}_{max_depth}"):
-    mlflow.autolog()
-    
-    model_forest = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    model_forest.fit(X_train, y_train)
-    
-    accuracy = model_forest.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+        mlflow.autolog()
+
+        model_forest = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model_forest.fit(X_train, y_train)
+        
+        accuracy = model_forest.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
